78afeec1341f00f9064ddbda3025948ba49cd950
BasmalaYasser <103972624+Basmalayaser@users.noreply.github.com>
2024-08-02 03:09:53 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <div className="bg_green text-white flex flex-col items-center justify-center py-20">
-        <img src='src/assets/avataaars.svg' alt="avataaar Img" className='w-60' />
+        <img src='https://raw.githubusercontent.com/Basmalayaser/Pro1InReact_Route/main/src/assets/avataaars.svg' alt="avataaar Img" className='w-60' />
       <h2 className='text-4xl font-bold pt-5'>Start Framework</h2>
       <div className={`my-10`}> <span className={`line bg-white`}></span><i class={`fa-solid fa-star text-2xl px-5 text-white`}></i> <span className={`line bg-white`}></span></div>
        <p className='text-lg'>Graphic Artist - Web Designer - Illustrator</p>
